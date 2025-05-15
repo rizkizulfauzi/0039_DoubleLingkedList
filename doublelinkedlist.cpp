a00492929b85c56e9d79624efcf46bc52a49cf34
rizkizulfauzi <rizkizulfauzi18@gmail.com>
2025-05-15 10:37:38 +0700

    Membuat Node baru

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -20,4 +20,15 @@ public:
     {
         START  = NULL;
     }
-};
+
+    void addNode(){
+        int nim;
+        string nama;
+        cout <<"\nEnter the roll number of the student";
+        cin >> nim;
+
+        Node *newNode = new Node();
+
+        newNode ->noMhs = nim;
+    }
+}; 
